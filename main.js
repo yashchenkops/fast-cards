c562fa73c59537008194427bda5a615253b07672
Pavel <yashchenkodev@gmail.com>
2026-03-11 18:39:38 +0200

    feat: add card render

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+function createCard(plan) {
+  const card = document.createElement('div');
+
+  card.className = 'card';
+  card.innerHTML = `
+    <div class="card__pricing">
+      ${plan.is_best ? '<div class="is-best">Best value</div>' : ''}
+      <span class="card__pricing-amount">$${plan.amount}</span>
+      /per year
+    </div>
+    <div class="card__name">
+      <p class="card__name-prod">${plan.name_prod}</p>
+      <p class="card__name-license">${plan.license_name}</p>
+    </div>
+    <div class="card__actions">
+      <a class="card__actions-download" href="${plan.link}">
+        Download
+      </a>
+    </div>
+  `;
+
+  return card;
+}
+
 async function fetchPlans() {
   const response = await fetch('https://veryfast.io/t/front_test_api.php');
   const data = await response.json();
